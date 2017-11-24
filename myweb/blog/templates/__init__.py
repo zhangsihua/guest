537793ab5d4ea170644bf89ba0b8e69f8a30d9a1
sihuazhang <sihuazhang@sohu-inc.com>
2017-11-24 15:58:25 +0800

    1

---
+++ myweb/blog/templates/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding:utf-8 -*-
+__author__ = "ZhangSiHua"
+__created_date__ = "2017/11/24"
--- myweb/blog/views.py
+++ myweb/blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+
+from blog.models import Blog
+from django.shortcuts import render_to_response
+
 
 # Create your views here.
+
+def index(request):
+    blog_list = Blog.objects.all()
+    return render_to_response('index.html', {'blogs': blog_list})
