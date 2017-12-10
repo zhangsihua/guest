673a0f012df1505ba2a2c162687c312d93463c29
zhangsihua <sihuazhang@sohu-inc.com>
2017-12-10 22:17:25 +0800

    12.10

--- sign/admin.py
+++ sign/admin.py
@@ -3,5 +3,17 @@ from sign.models import Event, Guest
 
 
 # Register your models here.
-admin.site.register(Event)
-admin.site.register(Guest)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'status', 'address', 'start_time']
+    search_fields = ['name']    # 搜索栏
+    list_filter = ['status']    # 过滤器
+
+
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ['realname', 'phone', 'email', 'sign', 'create_time', 'event']
+    search_fields = ['realname', 'phone']
+    list_filter = ['sign']
+
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Guest, GuestAdmin)
