65e0d38a979b5111c93e0d6635b8b11216288a06
zhangsihua <sihuazhang@sohu-inc.com>
2017-12-10 17:55:45 +0800

    12.10

--- guest/urls.py
+++ guest/urls.py
@@ -19,9 +19,9 @@ from sign import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    # url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url(r'^index/$', views.index),
     url(r'^accounts/login/$', views.index),
-    # url(r'^login_action/$', views.login_action),
+    url(r'^login_action/$', views.login_action),
     url(r'^event_manage/$', views.event_manage),
 ]
